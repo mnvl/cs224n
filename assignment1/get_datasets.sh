a55fe5b716d4a08c98ce878c0e8565d789a759a0
Manvel Avetisian <avetisian@gmail.com>
2017-04-28 12:37:56 +0300

    glove.6B.50d.txt.zip seems to be not available anymore -- fetch it from somewhere else

--- assignment1/get_datasets.sh
+++ assignment1/get_datasets.sh
@@ -16,9 +16,9 @@ rm stanfordSentimentTreebank.zip
 
 # Get 50D GloVe vectors
 if hash wget 2>/dev/null; then
-  wget http://web.stanford.edu/~jamesh93/tmp/glove.6B.50d.txt.zip
+  wget https://nlp.stanford.edu/data/glove.6B.zip
 else
-  curl -O http://web.stanford.edu/~jamesh93/tmp/glove.6B.50d.txt.zip
+  curl -O https://nlp.stanford.edu/data/glove.6B.zip
 fi
-unzip glove.6B.50d.txt.zip
-rm glove.6B.50d.txt.zip
+unzip glove.6B.zip
+rm glove.6B.zip
