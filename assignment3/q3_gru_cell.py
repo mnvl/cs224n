897cd354d53a22b5abda4c7c84b36d420c22e232
Manvel Avetisian <avetisian@gmail.com>
2017-05-04 21:08:26 +0300

    q3/gru cell is done

--- assignment3/q3_gru_cell.py
+++ assignment3/q3_gru_cell.py
@@ -65,7 +65,28 @@ class GRUCell(tf.contrib.rnn.RNNCell):
         # be defined elsewhere!
         with tf.variable_scope(scope):
             ### YOUR CODE HERE (~20-30 lines)
-            pass
+            U_z = tf.get_variable("U_z", (self.input_size, self._state_size), tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            W_z = tf.get_variable("W_z", (self._state_size, self._state_size), tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            b_z = tf.get_variable("b_z", (self._state_size,), tf.float32,
+                                  initializer=tf.zeros_initializer())
+            U_r = tf.get_variable("U_r", (self.input_size, self._state_size), tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            W_r = tf.get_variable("W_r", (self._state_size, self._state_size), tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            b_r = tf.get_variable("b_r", (self._state_size,), tf.float32,
+                                  initializer=tf.zeros_initializer())
+            U_o = tf.get_variable("U_o", (self.input_size, self._state_size), tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            W_o = tf.get_variable("W_o", (self._state_size, self._state_size), tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            b_o = tf.get_variable("b_o", (self._state_size,), tf.float32,
+                                  initializer=tf.zeros_initializer())
+            z_t = tf.nn.sigmoid(tf.matmul(inputs, U_z) + tf.matmul(state, W_z) + b_z)
+            r_t = tf.nn.sigmoid(tf.matmul(inputs, U_r) + tf.matmul(state, W_r) + b_r)
+            o_t = tf.nn.tanh(tf.matmul(inputs, U_o) + r_t * tf.matmul(state, W_o) + b_o)
+            new_state = z_t * state + (1. - z_t) * o_t
             ### END YOUR CODE ###
         # For a GRU, the output and state are the same (N.B. this isn't true
         # for an LSTM, though we aren't using one of those in our
@@ -110,6 +131,7 @@ def test_gru_cell():
 
                 y_, ht_ = session.run([y_var, ht_var], feed_dict={x_placeholder: x, h_placeholder: h})
                 print("y_ = " + str(y_))
+                print("ht = " + str(ht))
                 print("ht_ = " + str(ht_))
 
                 assert np.allclose(y_, ht_), "output and state should be equal."
