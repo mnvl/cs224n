98f4a8cf9afacb07f61dd24aef243b4dce38265b
Manvel Avetisian <avetisian@gmail.com>
2017-05-05 09:22:58 +0300

    Move all sources to assignment4/ from assignment4/code.

--- assignment4/get_started.sh
+++ assignment4/get_started.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 # Downloads raw data into ./download
 # and saves preprocessed data into ./data
-# Get directory containing this script
-
-CODE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export PYTHONPATH=$PYTHONPATH:$CODE_DIR
-
-pip install -r $CODE_DIR/requirements.txt
 
 # download punkt, perluniprops
 if [ ! -d "/usr/local/share/nltk_data/tokenizers/punkt" ]; then
     python2 -m nltk.downloader punkt
 fi
-
 
 # SQuAD preprocess is in charge of downloading
 # and formatting the data to be consumed later
@@ -21,10 +13,10 @@ DATA_DIR=data
 DOWNLOAD_DIR=download
 mkdir -p $DATA_DIR
 rm -rf $DATA_DIR
-python2 $CODE_DIR/preprocessing/squad_preprocess.py
+python2 ./preprocessing/squad_preprocess.py
 
 # Download distributed word representations
-python2 $CODE_DIR/preprocessing/dwr.py
+python2 ./preprocessing/dwr.py
 
 # Data processing for TensorFlow
-python2 $CODE_DIR/qa_data.py --glove_dim 100
+python2 ./qa_data.py --glove_dim 100
